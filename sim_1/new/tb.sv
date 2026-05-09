354e890d750816ef3901062b5fb995109e8b4784
ayeshax86 <ayesha86798@gmail.com>
2026-05-09 14:07:48 +0500

    handshake logic fixed only tdata stall logic left

--- sim_1/new/tb.sv
+++ sim_1/new/tb.sv
@@ -1,20 +1,12 @@
 `timescale 1ns/1ps
 
-<<<<<<< HEAD
 module tb;
-=======
-module tb_pgen;
->>>>>>> 76158a32d630998cfda3b0d1fe178409715e3ecc
 
     logic        clk;
     logic        rst_n;
 
     logic [10:0] pkt_size;
-<<<<<<< HEAD
     logic [15:0]  eth_type;
-=======
-    logic [1:0]  eth_type;
->>>>>>> 76158a32d630998cfda3b0d1fe178409715e3ecc
     logic [2:0]  n_pkt;
     logic [3:0]  ipg;
 
@@ -50,16 +42,12 @@ module tb_pgen;
 
     always #5 clk = ~clk;
 
-<<<<<<< HEAD
 
   
-=======
->>>>>>> 76158a32d630998cfda3b0d1fe178409715e3ecc
     initial begin
         clk = 0;
         rst_n = 0;
 
-<<<<<<< HEAD
         # 20;
         rst_n = 1;  
         tready = 1;
@@ -86,63 +74,10 @@ module tb_pgen;
         
         
         
-=======
-        pkt_size = 0;
-        eth_type = 0;
-        n_pkt    = 0;
-        ipg      = 0;
-
-        trig     = 0;
-
-        src_addr = 0;
-        dst_addr = 0;
-
-        tready   = 1;
-
-        #20;
-        rst_n = 1;
-
-        #20;
-
-        // same packet parameters
-        pkt_size = 10;
-        n_pkt    = 3;
-        ipg      = 2;
-
-        eth_type = 2'b00;
-
-        src_addr = 48'h112233445566;
-        dst_addr = 48'hAABBCCDDEEFF;
-
-        // packet 1
-        trig = 1;
-        #10;
-        trig = 0;
-
-        #300;
-
-        // packet 2
-        trig = 1;
-        #10;
-        trig = 0;
-
-        #300;
-
-        // packet 3
-        trig = 1;
-        #10;
-        trig = 0;
-
-        #500;
->>>>>>> 76158a32d630998cfda3b0d1fe178409715e3ecc
 
         $finish;
     end
 
-<<<<<<< HEAD
 endmodule
 
 
-=======
-endmodule
->>>>>>> 76158a32d630998cfda3b0d1fe178409715e3ecc
